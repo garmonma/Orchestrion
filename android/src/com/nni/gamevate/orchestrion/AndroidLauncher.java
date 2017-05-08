4cb73f5c28cc66e7be17d83b5bbd461e8d088011
garmonma <garmonmarcus@gmail.com>
2017-05-07 18:19:48 -0700

    Initial commit

---
+++ android/src/com/nni/gamevate/orchestrion/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.nni.gamevate.orchestrion;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.nni.gamevate.orchestrion.Orchestrion;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new Orchestrion(), config);
+	}
+}
---
+++ desktop/src/com/nni/gamevate/orchestrion/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.nni.gamevate.orchestrion.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.nni.gamevate.orchestrion.Orchestrion;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Orchestrion(), config);
+	}
+}
